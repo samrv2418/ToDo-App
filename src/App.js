324618bb751b01848091748699b3a5b81520fc2f
samrv2418 <samrv2418@gmail.com>
2022-06-18 17:55:14 +0530

    samp

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React and Have fun
+          Learn React and Have fun with
         </a>
       </header>
     </div>
