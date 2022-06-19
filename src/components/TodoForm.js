458a37da8d9f68f216c589486a7e67d21020871f
samrv2418 <samrv2418@gmail.com>
2022-06-19 16:29:06 +0530

    Basic Structure

---
+++ src/components/TodoForm.js
@@ -0,0 +1,10 @@
+// rfce ==> shortcut cmd to create structure / function that you can see below.
+import React from 'react'
+
+function TodoForm() {
+  return (
+    <div>TodoForm</div>
+  )
+}
+
+export default TodoForm
